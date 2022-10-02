9a5a8d5208959a4482c38383439433c89293178a
Denis <Denis120179@yandex.by>
2022-10-02 13:33:32 +0300

    Первая задача

--- Program.cs
+++ Program.cs
@@ -1,1 +1,17 @@
-﻿
+﻿/*
+Задача 1
+Напишите программу, которая на вход принимает два числа и выдает, какое число большее
+*/
+
+Console.WriteLine("Введите первое число:  ");
+int a = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите второе число:  ");
+int b = int.Parse(Console.ReadLine()!);
+if(a>b)
+{
+    Console.WriteLine($"{a} больше {b}");
+}
+else 
+{
+    Console.WriteLine($"{b} больше {a}");
+}
