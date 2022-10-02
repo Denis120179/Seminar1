c40abe22db62c7c59cce80153836c5d89c975a2f
Denis <Denis120179@yandex.by>
2022-10-02 13:48:38 +0300

    Вторая задача

--- Program.cs
+++ Program.cs
@@ -3,7 +3,7 @@
 Напишите программу, которая на вход принимает два числа и выдает, какое число большее
 */
 
-Console.WriteLine("Введите первое число:  ");
+/*Console.WriteLine("Введите первое число:  ");
 int a = int.Parse(Console.ReadLine()!);
 Console.WriteLine("Введите второе число:  ");
 int b = int.Parse(Console.ReadLine()!);
@@ -14,4 +14,38 @@ if(a>b)
 else 
 {
     Console.WriteLine($"{b} больше {a}");
+}*/
+
+/*
+Задача 2
+Напишите программу, которая принимает на вход три числа и выдает максимальное из этих чисел
+*/
+
+Console.WriteLine("Введите первое число:  ");
+int a = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите второе число:  ");
+int b = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите третье число:  ");
+int c = int.Parse(Console.ReadLine()!);
+if(a>b)
+{
+    if(a>c)
+    {
+        Console.WriteLine($"{a} максимальное число");            
+    }
+    else
+    {
+        Console.WriteLine($"{c} максимальное число");
+    }
 }
+else
+{
+    if(b>c)
+    {
+        Console.WriteLine($"{b} максимальное число");        
+    }
+    else
+    {
+        Console.WriteLine($"{c} максимальное число");
+    }
+} 
